7a28ec000c8291612a8e8a479f3feaf725f54e76
2celeb <oooooorz@gmail.com>
2012-06-25 13:43:01 +0900

    oops

--- js/user-edit.js
+++ js/user-edit.js
@@ -1,18 +1,3 @@
 $(function() {
 
-  $("#get_name").click(function(){
-    $("#subject01_result").html($("#yamada_name").html());
-
-  });
-
-  $("#add_sakurako").click(function(){
-    $('#member_list').prepend('<li>櫻子</li>');
-  });
-
-  $("#add_himawari").click(function(){
-    $('#member_list').prepend('<li>向日葵</li>');
-  });
-
-
-
 });;
